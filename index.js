3157034c4ab5d551913b2517da41394512cc94d8
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-22 18:01:31 +0100

    finished diceGame

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1;
-document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+    document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
 var randomNumber2 = Math.floor(Math.random()*6)+1;
-document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+    document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+if(randomNumber1 == randomNumber2){
+  document.querySelector("h1").innerHTML += ("<a href='index.html'>Draw, Play Again?</a>");
+}
+else if (randomNumber1>randomNumber2) {
+  document.querySelector("h1").innerHTML += ("<a href='index.html'>Player 1 Wins, Play Again?</a>");
+}
+else if (randomNumber1<randomNumber2) {
+  document.querySelector("h1").innerHTML += ("<a href='index.html'>Player 2 Wins, Play Again?</a>");
+}
