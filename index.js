f91eece1a0431e9b5b37cc691730185a336eabee
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-23 11:58:34 +0100

    formatting changes

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomNumber2 = Math.floor(Math.random()*6)+1;
     document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
 
+
+
+
 //series of if statements that compare the random numbers generated then changes the content
 //of the h1 to declare a winner or a draw the h1 is nested with an anchor tag so it be clicked to refresh
 //the page and play Again
-
-
 if(randomNumber1 == randomNumber2){
   document.querySelector("h1").innerHTML += ("<a href='index.html'>Draw, Play Again?</a>");
 }
