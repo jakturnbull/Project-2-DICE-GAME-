a2132c88b9ce5dbbd36d524bc6fed528149acb5d
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-23 11:55:30 +0100

    added comments

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+//generates a random number between 1 and 6
 var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+//finds the object using querySelector in this case an image and then changes the text of that attributes randomNumber1
+// eg dice+1+.png so everytime the website is refreshed a new dice image appears
     document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+
+//replicating the process above but for the second image
 var randomNumber2 = Math.floor(Math.random()*6)+1;
     document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+
+//series of if statements that compare the random numbers generated then changes the content
+//of the h1 to declare a winner or a draw the h1 is nested with an anchor tag so it be clicked to refresh
+//the page and play Again
+
+
 if(randomNumber1 == randomNumber2){
   document.querySelector("h1").innerHTML += ("<a href='index.html'>Draw, Play Again?</a>");
 }
